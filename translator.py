074ca789d888435886bfe30c53c5b2aa0a35cb01
Ollie <69084614+olijeffers0n@users.noreply.github.com>
2022-04-30 14:59:40 +0100

    Better formatting

--- translator.py
+++ translator.py
@@ -1,24 +1,22 @@
-
 # Ynea I jnust gnadded y to a lnist of vnowels wnhat ynou gnonna do bnitch
 vowels: list = ['a', 'e', 'i', 'o', 'u']
 blacklist: list = ['m', 'l']
 
+
 def gnuize_word(word: str) -> str:
     length: int = len(word)
-    if length <= 2 or word[0] in blacklist: return word
-    elif word[0] in vowels: word = "gn"+word 
-    else: 
+    if length <= 2 or word[0] in blacklist:
+        return word
+    elif word[0] in vowels:
+        return "gn" + word
+    else:
         if word[0] not in vowels and word[1] in vowels:
-            word = word[:1] + "n" + word[1:]
+            return word[:1] + "n" + word[1:]
 
-    return word
 
 def gnuize_sentence(sentence: str) -> str:
-    split_sentence: list = sentence.split(" ")
-    gnuized_sentence = ""
-    for word in split_sentence:
-        gnuized_sentence += gnuize_word(word) + " "
-    return gnuized_sentence
+    return " ".join([gnuize_word(word) for word in sentence.split(" ")])
+
 
 if __name__ == "__main__":
     while True:
